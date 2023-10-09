051a06e862d823ac90a64bed8ed3b3c15d91057d
SeveratheDuck <antipov.aa@phystech.edu>
2023-10-09 14:12:11 +0300

    Version 1. Errors added. No stack dump.

--- SPU.cpp
+++ SPU.cpp
@@ -1,30 +1,53 @@
 #include "headers/SPU.h"
 
-void SPU_CTOR (SPU_struct* const spu)
+SPU_error SPU_CTOR (SPU_struct* const spu)
 {
     assert (spu);
-    assert (&spu->stk);
     assert (spu->register_vars);
 
     STACK_CTOR (&spu->stk);
 
-    for (size_t i = 0; i < reg_number; ++i)
+    for (size_t i = 0; i < REG_NUMBER; ++i)
     {
         spu->register_vars[i] = 0;
     }
+
+    spu->spu_errors_list = {};
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_DTOR (SPU_struct* spu)
+SPU_error SPU_DTOR (SPU_struct* spu)
 {
-    assert  (spu);
-    assert (&spu->stk);
+    SPU_VERIFY (spu);
 
     STACK_DTOR (&spu->stk);
 
-    for (size_t i = 0; i < reg_number; ++i)
+    for (size_t i = 0; i < REG_NUMBER; ++i)
     {
         spu->register_vars[i] = 0;
     }
+
+    spu->spu_errors_list = {};
+
+    return spu->spu_errors_list;
+}
+
+SPU_error SPU_verify (SPU_struct* spu)
+{
+    if (!spu)
+    {
+        SPU_error spu_nullptr = { 1 };
+        return spu_nullptr;
+    }
+
+    if (StackVerify (&spu->stk))
+    {
+        spu->spu_errors_list.SPU_STACK_ERR = 1;
+    }
+
+    return spu->spu_errors_list;
 }
 
 void SPU_process (const char* const spu_file_name)
@@ -43,11 +66,11 @@ void SPU_process (const char* const spu_file_name)
     SPU_DTOR (&spu);
 }
 
-void Processor (file_input* const asm_code,
-                SPU_struct* const spu)
+SPU_error Processor (file_input* const asm_code,
+                     SPU_struct* const spu)
 {
     assert (asm_code);
-    assert (spu);
+    SPU_VERIFY (spu);
 
     int n_operation = 0;
 
@@ -57,7 +80,7 @@ void Processor (file_input* const asm_code,
         switch (n_operation)
         {
             case ASM_HLT:
-                return;
+                return spu->spu_errors_list;
                 break;
 
             case ASM_PUSH:
@@ -97,13 +120,18 @@ void Processor (file_input* const asm_code,
                 break;
         }
     }
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_push (SPU_struct* const spu,
+SPU_error SPU_push (SPU_struct* const spu,
                file_input* const asm_code,
                const size_t n_line,
                int n_operation)
 {
+    SPU_VERIFY (spu);
+
     int push_regime = STANDART_REGIME;
     int push_value = 0;
 
@@ -118,52 +146,80 @@ void SPU_push (SPU_struct* const spu,
     {
         StackPush (&spu->stk, spu->register_vars[push_value]);
     }
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_add (SPU_struct* const spu)
+SPU_error SPU_add (SPU_struct* const spu)
 {
+    SPU_VERIFY (spu);
+
     int l_operator  = 0;
     int r_operator  = 0;
 
     StackPop  (&spu->stk, &r_operator);
     StackPop  (&spu->stk, &l_operator);
     StackPush (&spu->stk, l_operator + r_operator);
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_sub (SPU_struct* const spu)
+SPU_error SPU_sub (SPU_struct* const spu)
 {
+    SPU_VERIFY (spu);
+
     int l_operator  = 0;
     int r_operator  = 0;
 
     StackPop  (&spu->stk, &r_operator);
     StackPop  (&spu->stk, &l_operator);
     StackPush (&spu->stk, l_operator - r_operator);
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_mul (SPU_struct* const spu)
+SPU_error SPU_mul (SPU_struct* const spu)
 {
+    SPU_VERIFY (spu);
+
     int l_operator  = 0;
     int r_operator  = 0;
 
     StackPop  (&spu->stk, &r_operator);
     StackPop  (&spu->stk, &l_operator);
     StackPush (&spu->stk, l_operator * r_operator);
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_div (SPU_struct* const spu)
+SPU_error SPU_div (SPU_struct* const spu)
 {
+    SPU_VERIFY (spu);
+
     int l_operator  = 0;
     int r_operator  = 0;
 
     StackPop  (&spu->stk, &r_operator);
     StackPop  (&spu->stk, &l_operator);
     StackPush (&spu->stk, l_operator / r_operator);
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
 
-void SPU_out (SPU_struct* const spu)
+SPU_error SPU_out (SPU_struct* const spu)
 {
+    SPU_VERIFY (spu);
+
     int output_value = 0;
 
     StackPop (&spu->stk, &output_value);
     printf ("%d", output_value);
+
+    SPU_VERIFY (spu);
+    return spu->spu_errors_list;
 }
--- headers/SPU.h
+++ headers/SPU.h
@@ -5,36 +5,62 @@
 #include "../stack/headers/stack.h"
 #include "translator.h"
 
-const size_t reg_number = 4;
+#define SPU_VERIFY(spu)                         \
+    SPU_verify (spu);                           \
+    if (spu)                                    \
+    {                                           \
+        if (*(int*) &spu->spu_errors_list != 0) \
+        {                                       \
+            return spu->spu_errors_list;        \
+        }                                       \
+    }                                           \
+    else                                        \
+    {                                           \
+        SPU_error spu_nullptr = { 1 };          \
+        return spu_nullptr;                     \
+    }
+
+const size_t REG_NUMBER = 4;
+
+struct SPU_error
+{
+    int SPU_NULLPTR                : 1;
+    int SPU_FILE_WRONG_DECODE_WORD : 1;
+    int SPU_STACK_ERR              : 11;
+    int SPU_WRONG_INPUT_COMMAND    : 1;
+};
 
 struct SPU_struct
 {
     Stack stk;
-    int register_vars[reg_number];
+    int register_vars[REG_NUMBER];
+    SPU_error spu_errors_list;
 };
 
-void SPU_СTOR (SPU_struct* spu);
+SPU_error SPU_СTOR (SPU_struct* spu);
 
-void SPU_DTOR (SPU_struct* spu);
+SPU_error SPU_DTOR (SPU_struct* spu);
+
+SPU_error SPU_verify (SPU_struct* spu);
 
 void SPU_process (const char* const spu_file_name);
 
-void Processor (file_input* const asm_code,
+SPU_error Processor (file_input* const asm_code,
                 SPU_struct* const spu);
 
-void SPU_push (SPU_struct* const spu,
+SPU_error SPU_push (SPU_struct* const spu,
                file_input* const asm_code,
                const size_t n_line,
                int n_operation);
 
-void SPU_add  (SPU_struct* const spu);
+SPU_error SPU_add  (SPU_struct* const spu);
 
-void SPU_sub  (SPU_struct* const spu);
+SPU_error SPU_sub  (SPU_struct* const spu);
 
-void SPU_mul  (SPU_struct* const spu);
+SPU_error SPU_mul  (SPU_struct* const spu);
 
-void SPU_div  (SPU_struct* const spu);
+SPU_error SPU_div  (SPU_struct* const spu);
 
-void SPU_out  (SPU_struct* const spu);
+SPU_error SPU_out  (SPU_struct* const spu);
 
 #endif
